68de527ae94fd2b8fc0a50232961f958d69daa1b
muqeetqazi <muqeetahmad202@gmail.com>
2025-06-22 22:21:45 +0500

    fix

--- core/settings.py
+++ core/settings.py
@@ -64,11 +64,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'protectedvision',
+        'USER': 'pvuser',
+        'PASSWORD': '?Muqeet@12Qazi',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
